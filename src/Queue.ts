4460ab169f65fdaea74818e0bb4d522a4e0f1570
Andres Castillo <info@acastillo.dev>
2024-06-08 08:17:13 -0500

    Change queue implementation to be generic

--- src/Queue.ts
+++ src/Queue.ts
@@ -1,16 +1,16 @@
-class QueueNode {
-    value: number
-    next: QueueNode | null
+class QueueNode<T> {
+    value: T
+    next: QueueNode<T> | null
 
-    constructor(value: number) {
+    constructor(value: T) {
         this.value = value
         this.next = null
     }
 }
 
-class Queue {
-    head: QueueNode | null
-    tail: QueueNode | null
+class Queue<T> {
+    head: QueueNode<T> | null
+    tail: QueueNode<T> | null
     length: number
 
     constructor() {
@@ -19,7 +19,7 @@ class Queue {
         this.length = 0
     }
 
-    enqueue(node: QueueNode): void {
+    enqueue(node: QueueNode<T>): void {
         if (!this.tail) {
             this.tail = node
             this.head = node
@@ -30,7 +30,7 @@ class Queue {
         this.length += 1
     }
 
-    deque(): number | null {
+    deque(): T | null {
         if (!this.head) {
             return null
         }
@@ -48,15 +48,38 @@ class Queue {
     }
 }
 
-const queue = new Queue()
+interface Persona {
+    name: string;
+    username?: string;
+    email: string;
+}
+
+const persona1: Persona = {
+    name: 'juan',
+    email: 'juan@mail.com'
+}
+
+const persona2: Persona = {
+    name: 'maria',
+    username: 'juan01',
+    email: 'juan@mail.com'
+}
+
+const persona3: Persona = {
+    name: 'luis',
+    username: 'juan01',
+    email: 'juan@mail.com'
+}
+
+const queue = new Queue<Persona>()
 console.log(queue)
-const n1 = new QueueNode(1)
+const n1 = new QueueNode<Persona>(persona1)
 queue.enqueue(n1)
 console.log(queue)
-const n2 = new QueueNode(2)
+const n2 = new QueueNode<Persona>(persona2)
 queue.enqueue(n2)
 console.log(queue)
-const n3 = new QueueNode(3)
+const n3 = new QueueNode<Persona>(persona3)
 queue.enqueue(n3)
 console.log(JSON.stringify(queue))
 let value = queue.deque()
